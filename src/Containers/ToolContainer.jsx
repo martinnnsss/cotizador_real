091d543df4a02cc3866a1ca42f9cfe8bc766611d
martinnnsss <martin.olivares@cabify.com>
2024-05-16 11:39:42 -0400

    production label

--- src/Containers/ToolContainer.jsx
+++ src/Containers/ToolContainer.jsx
@@ -240,7 +240,7 @@ function ToolContainer({ type }){
             const link = document.createElement('a');
             link.href = url;
 
-            const cotizacionName ='Cotizacion2'+fileName
+            const cotizacionName ='Cotizacion'+fileName
             
             link.setAttribute('download', cotizacionName);
             document.body.appendChild(link);
